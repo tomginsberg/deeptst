e0963505a9e3b8c3a4f10514911f62a16a5ad79d
Tom Ginsberg <tomginsberg@cs.toronto.edu>
2022-11-15 16:06:00 -0500

    self contained proj norm script

--- scratch/proj_norm/cifar.py
+++ scratch/proj_norm/cifar.py
@@ -1,19 +1,69 @@
+from os.path import join
+
 import numpy as np
-
-from data import sample_data
-from data.core import split_dataset
-from models import pretrained
 import pandas as pd
+import torch.nn
+import torchvision
+from torch.utils.data import TensorDataset, random_split
+from torchvision.transforms import Compose, Normalize, RandomCrop, RandomHorizontalFlip, ToTensor
 
-model = pretrained.resnet18_trained_on_cifar10().model  # loads a resnet 18 model pretrained on cifar 10
-model.eval()
+MEAN, STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
 
-p_train, p_val, p_test_all = sample_data.cifar10(split='all')  # loads the cifar 10 dataset
-# above model was trained on p_train + p_val
 
-q_all = sample_data.cifar10_1()  # loads the cifar 10_1 dataset
+def split_dataset(dataset, num_samples: int, random_seed: int = 42):
+    # noinspection PyTypeChecker
+    return random_split(dataset, [num_samples, len(dataset) - num_samples],
+                        generator=torch.Generator().manual_seed(random_seed))
 
-test_sets = {'p': p_test_all, 'q': q_all}
+
+def get_mean_std(normalize=True):
+    if normalize:
+        return MEAN, STD
+    else:
+        return (0, 0, 0), (1, 1, 1)
+
+
+def cifar10(root='/voyager/datasets', split='train', normalize=True, augment=False):
+    if split not in ['train', 'val', 'test', 'all']:
+        raise ValueError(f'Invalid split: {split}')
+
+    transform = []
+    if augment:
+        transform.append(RandomCrop(32, padding=4))
+        transform.append(RandomHorizontalFlip())
+
+    transform.append(ToTensor())
+    if normalize:
+        transform.append(Normalize(MEAN, STD))
+
+    train, rest = [torchvision.datasets.CIFAR10(
+        root=root,
+        train=x,
+        download=False,
+        transform=Compose(transform)
+    ) for x in [True, False]]
+
+    if split != 'train':
+        val, test = split_dataset(rest, num_samples=int(len(rest) * 9 / 10), random_seed=0)
+        if split == 'val':
+            return val
+        elif split == 'test':
+            return test
+        return train, val, test
+    return train
+
+
+def cifar10_1(root='/voyager/datasets/cifar-10-1', normalize=True, format_spec='list'):
+    data = torch.from_numpy(np.load(join(root, 'cifar10.1_v6_data.npy'))) / 255
+    data = data.permute(0, 3, 1, 2)
+    if normalize:
+        data = data.sub(torch.tensor(MEAN).view(3, 1, 1)).div(torch.tensor(STD).view(3, 1, 1))
+    labels = np.load(join(root, 'cifar10.1_v6_labels.npy'))
+    labels = torch.from_numpy(labels).long()
+    if format_spec == 'tensor_dataset':
+        return TensorDataset(data, labels)
+    if format_spec == 'list':
+        return [(d, l.item()) for d, l in zip(data, labels)]
 
 
 def compute_proj_norm(model, dataset):
@@ -22,10 +72,24 @@ def compute_proj_norm(model, dataset):
 
 
 if __name__ == '__main__':
+
     # parameters for the experiment
     samples = [10, 20, 50]
     seeds = 100
     alpha = 0.05
+    checkpoint_path = '/voyager/projects/tomginsberg/checkpoints/resnet18_cifar.pt'
+
+    model = torchvision.models.resnet18()
+    model.fc = torch.nn.Linear(model.fc.in_features, 10)
+    print(model.load_state_dict(torch.load(checkpoint_path)))
+    model.eval()
+
+    p_train, p_val, p_test_all = cifar10(split='all', normalize=True)  # loads the cifar 10 dataset
+    # above model was trained on p_train + p_val
+
+    q_all = cifar10_1(normalize=True)  # loads the cifar 10_1 dataset
+
+    test_sets = {'p': p_test_all, 'q': q_all}
 
     proj_norms = {x: {'p': [], 'q': []} for x in samples}
     for N in samples:
